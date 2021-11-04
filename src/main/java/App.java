67917ab0e685398a31d19cf3609e3142f69ad891
Mykola <klopina@i.ua>
2021-11-04 06:52:09 +0200

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,7 @@ public class App {
     public static void main(String[] args) {
         ApiContextInitializer.init();
 //        Bot test_my_gor4i4nick_bot = new Bot("test_my_gor4i4nick_bot", "1574453505:AAF5WUoI_P7mfnsE77ZFLr8bbxRc2gVhgFI");
-        Bot klopinabot = new Bot("klopinabot", "1363999060:AAETuTK_i_xonIfZnlviFh-dTAMuGYcI4Ak");
+        Bot klopinabot = new Bot("@klopinabot", "1363999060:AAETuTK_i_xonIfZnlviFh-dTAMuGYcI4Ak");
         klopinabot.botConnect();
     }
 }
